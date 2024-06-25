e33199882688093dcb39ed42b66bb29714a0ffac
Renato <renatodev404@gmail.com>
2024-06-25 09:44:03 -0300

    Atualizando articles com uma partial form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+    #esses comandos aqui embaixo estão dizendo que os campos title e body são obrigatórios
+    #para ninguém ficar postando campo vazio
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :body, presence: true, length: { minimum: 10 }
 end
+
+
